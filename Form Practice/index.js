8990ccd10570fd4784a9b43fb547517385a60788
Monkey Hacker <111luisito111@gmail.com>
2020-04-25 14:54:32 +0200

    init todolist 2

--- Form Practice/index.js
+++ Form Practice/index.js
@@ -2,3 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-doom'
 import App from './App.js'
 ReactDOM.render(<App />,document.getElementById("root"))
+
+{/*https://scrimba.com/p/p7P5Hd/cRqP9mhg*/}
