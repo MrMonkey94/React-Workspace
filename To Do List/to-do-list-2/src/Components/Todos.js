2c1aeb4cede05eb9fcd0cc788ab049d3a272f3fa
Monkey Hacker <111luisito111@gmail.com>
2020-04-25 18:09:46 +0200

    components

---
+++ To Do List/to-do-list-2/src/Components/Todos.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Todos extends Component {
+render () {
+    return (
+        <div className="App">
+            <h1>App</h1>
+
+        </div>
+    )
+}
+}
+
+export default App;
